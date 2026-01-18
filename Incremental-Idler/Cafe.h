448cc63e21ec316e9d2822aa0d0f82394ae61f49
Adam <adam.a.cummins@gmail.com>
2026-01-18 13:40:18 +1000

    updated Cafe.h

--- Incremental-Idler/Cafe.h
+++ Incremental-Idler/Cafe.h
@@ -1,29 +1,8 @@
 #pragma once
-#include <SFML/Graphics.hpp>
-#include <iostream>
+#include "Building.h"
 
-class Cafe
+class Cafe : public Building
 {
 public:
 	Cafe(float x, float y, const sf::Texture& texture);
-
-	void draw(sf::RenderWindow& window);
-	bool isClicked(sf::Vector2f mousePos);
-	void shrink();
-	void resetScale();
-
-	void purchase();
-	long long getCost();
-	int getOwnedCount();
-	int getIncomePerSecond();
-	void setOwnedCount(int count);
-	void setCost(long long newCost);
-	float getIncreaseRate();
-
-private:
-	sf::Sprite sprite;
-	long long cost;
-	int ownedCount;
-	int baseIncome;
-	float increaseRate = 1.15f; // 15% increase rate
 };
