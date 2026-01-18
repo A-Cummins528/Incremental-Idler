959862b0bd03d8c057af596a9a3bc9a46f68ec5b
Adam <adam.a.cummins@gmail.com>
2026-01-18 13:38:14 +1000

    added Building.cpp base class

---
+++ Incremental-Idler/Building.cpp
@@ -0,0 +1,68 @@
+#include "Building.h"
+
+Building::Building(float x, float y, const sf::Texture& texture, long long baseCost, int incomeAmt)
+{
+	sprite.setTexture(texture);
+	sprite.setPosition(x, y);
+
+	// Default scale
+	sprite.setScale(1.0f, 1.0f);
+	originalScale = sf::Vector2f(1.0f, 1.0f);
+
+	// Centre Origin
+	sf::FloatRect bounds = sprite.getLocalBounds();
+	sprite.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
+
+	// Stats
+	cost = baseCost;
+	baseIncome = incomeAmt;
+	ownedCount = 0;
+	increaseRate = 1.15f; // 15% increase rate
+}
+
+void Building::draw(sf::RenderWindow& window)
+{
+	window.draw(sprite);
+}
+
+bool Building::isClicked(sf::Vector2f mousePos)
+{
+	return sprite.getGlobalBounds().contains(mousePos);
+}
+
+void Building::purchase()
+{
+	ownedCount++;
+	cost = (cost * increaseRate);
+}
+
+long long Building::getCost()
+{
+	return cost;
+}
+
+int Building::getOwnedCount()
+{
+	return ownedCount;
+}
+
+long long Building::getIncomePerSecond()
+{
+	return (long long)ownedCount * baseIncome;
+}
+
+void Building::setOwnedCount(int count)
+{
+	ownedCount = count;
+}
+
+void Building::shirnk()
+{
+	// Shrink to 90% of original scale
+	sprite.setScale(originalScale.x * 0.9f, originalScale.y * 0.9f);
+}
+
+void Building::resetScale()
+{
+	sprite.setScale(originalScale);
+}
---
+++ Incremental-Idler/Building.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class Building
+{
+public:
+	// Constructor
+	Building(float x, float y, const sf::Texture& texture, long long baseCost, int incomeAmt);
+
+	// Virtual Destructor
+	virtual ~Building() = default;
+
+	// --- SHARED FUNCTIONS ---
+	void draw(sf::RenderWindow& window);
+	bool isClicked(sf::Vector2f mousePos);
+
+	// Shop Logic
+	void purchase();
+	long long getCost();
+	int getOwnedCount();
+	long long getIncomePerSecond();
+
+	// Load Logic
+	void setOwnedCount(int count);
+
+	// Visuals
+	void shirnk();
+	void resetScale();
+
+protected:
+	sf::Sprite sprite;
+
+	long long cost;
+	int ownedCount;
+	int baseIncome;
+	float increaseRate;
+
+	// Helper to keep track of the original scale
+	sf::Vector2f originalScale;
+};
+
