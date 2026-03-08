8ec686bfdf803b58f39533450fd5383730b5fc8c
Adam <adam.a.cummins@gmail.com>
2026-03-08 15:22:19 +1000

    finished factory class files

--- Incremental-Idler/Factory.cpp
+++ Incremental-Idler/Factory.cpp
@@ -1,1 +1,9 @@
 #include "Factory.h"
+Factory::Factory(float x, float y, const sf::Texture& texture)
+	: Building(x, y, texture, 1000000, 10000)
+{
+}
+
+std::string Factory::getClassName() const {
+	return "Factory";
+}
